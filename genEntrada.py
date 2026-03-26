b83b7da635686d64ebf002f91871158927c23bc2
Mariana909 <karla.duque01@usa.edu.co>
2026-03-26 16:55:51 -0500

    Modificación genEntrada para generar una entrada más pequeña

--- genEntrada.py
+++ genEntrada.py
@@ -1,6 +1,6 @@
 with open("entrada.txt", "w") as f:
     base = "a b bas"
     
-    for i in range(1, 334):  # 3 * 333 = 999 tokens
+    for i in range(1, 51):  # 3 * 333 = 999 tokens
         linea = " ".join([base]*i)
         f.write(linea + "\n")
